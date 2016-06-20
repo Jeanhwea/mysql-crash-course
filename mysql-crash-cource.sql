fa9ea184583e65f6bc3d11b27cfbd3d518f17df5
Jinghui Hu <hujinghui@buaa.edu.cn>
2016-06-20 23:35:33 +0800

    add all titles

--- mysql-crash-cource.sql
+++ mysql-crash-cource.sql
@@ -1,6 +1,5 @@
 -- vim: set ts=2 sts=2 sw=2 ft=mysql:
 USE gold;
-
 -- create table
 CREATE TABLE `price` (
   `idprice` int(11) NOT NULL AUTO_INCREMENT,
@@ -11,7 +10,6 @@ CREATE TABLE `price` (
   `close` float NOT NULL,
   PRIMARY KEY (`idprice`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE `event` (
   `idevent` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
@@ -21,19 +19,13 @@ CREATE TABLE `event` (
   `volume` int(11) NOT NULL,
   PRIMARY KEY (`idevent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-
-
 SELECT `idprice`, `date`, `open`, `high`, `low`, `close`
   FROM gold.price
   LIMIT 5;
-
 SELECT `idprice`, `date`, `open`, `high`, `low`, `close`
   FROM gold.price
   LIMIT 3, 5;
-
 SELECT DISTINCT `scope_id`
   FROM gold.event;
-
 SELECT DISTINCT `scope_id`, `event_type_id`
   FROM gold.event;
